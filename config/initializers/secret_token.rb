889da84574d7e86817900120ead5d7fe9849429b
Taylor Jackson <tsjackson@gmail.com>
2011-06-04 15:01:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TemplateRails3::Application.config.secret_token = '294f279baab680edafe63cfccdba200117e42f464a42981c3997367996f91a91c6319b544ba82c11c5cd13e86028d01afc40471e1ee8f101a9440a011a2a4365'
